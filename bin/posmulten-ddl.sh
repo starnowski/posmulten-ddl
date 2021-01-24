2b940327408859814e0e558beb83dd62e95aeffd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-24 19:43:00 +0100

    Added vars file.

--- bin/posmulten-ddl.sh
+++ bin/posmulten-ddl.sh
@@ -6,8 +6,10 @@ else
     SCRIPT_DIR="$(dirname $0)"
 fi
 
+source "$SCRIPT_DIR/vars.sh"
+
 CREATE_SCRIPT_PATH="$2"
 DROP_SCRIPT_PATH="$3"
-JAR_FILE_PATH="$SCRIPT_DIR/../lib/configuration-jar-0.3.0-jar-with-dependencies.jar"
+JAR_FILE_PATH="$SCRIPT_DIR/../lib/configuration-jar-${POSMULTEN_JAR_FILE_VERSION}-jar-with-dependencies.jar"
 
 java -Dposmulten.configuration.config.file.path="$1" -Dposmulten.configuration.create.script.path="$CREATE_SCRIPT_PATH" -Dposmulten.configuration.drop.script.path="$DROP_SCRIPT_PATH" -jar "$JAR_FILE_PATH"
