ca710c8d30abf9e00cee462dcb653e01969b0731
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-24 19:15:29 +0100

    Added implementation

---
+++ bin/posmulten-ddl.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $0 != $BASH_SOURCE ]]; then
+    SCRIPT_DIR="$BASH_SOURCE"
+else
+    SCRIPT_DIR="$0"
+fi
+
+CREATE_SCRIPT_PATH="$2"
+DROP_SCRIPT_PATH="$3"
+JAR_FILE_PATH="$SCRIPT_DIR/../lib/configuration-jar-0.3.0-jar-with-dependencies.jar"
+
+java -Dposmulten.configuration.config.file.path="$1" -Dposmulten.configuration.create.script.path="$CREATE_SCRIPT_PATHl" -Dposmulten.configuration.drop.script.path="$DROP_SCRIPT_PATH" -jar "$JAR_FILE_PATH"
