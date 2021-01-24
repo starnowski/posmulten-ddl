848d0df159d1f6a484873102ad01634d98ba0da0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-24 22:17:32 +0100

    Added options.

--- bin/posmulten-ddl.sh
+++ bin/posmulten-ddl.sh
@@ -8,8 +8,49 @@ fi
 
 source "$SCRIPT_DIR/vars.sh"
 
-CREATE_SCRIPT_PATH="$2"
-DROP_SCRIPT_PATH="$3"
+function print_usage
+{
+cat << EOF
+USAGE:
+
+EOF
+}
+
+
+
+# Call getopt to validate the provided input.
+options=$(getopt -o "h" --long help,createSriptPath,dropScripPath: -- "$@")
+[ $? -eq 0 ] || {
+    echo "Incorrect options provided"
+    exit 1
+}
+
+CURRENT_DIR=`pwd`
+CREATE_SCRIPT_PATH="${CURRENT_DIR}/create_script.sql"
+DROP_SCRIPT_PATH="${CURRENT_DIR}/drop_script.sql"
+eval set -- "$options"
+while true; do
+    case "$1" in
+    --h,help)
+        print_usage
+        exit 0
+        ;;
+    --createSriptPath)
+        shift;
+        CREATE_SCRIPT_PATH="$1"
+        ;;
+    --dropScripPath)
+        shift;
+        DROP_SCRIPT_PATH="$1"
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
+
 JAR_FILE_PATH="$SCRIPT_DIR/../lib/configuration-jar-${POSMULTEN_JAR_FILE_VERSION}-jar-with-dependencies.jar"
 
 java -Dposmulten.configuration.config.file.path="$1" -Dposmulten.configuration.create.script.path="$CREATE_SCRIPT_PATH" -Dposmulten.configuration.drop.script.path="$DROP_SCRIPT_PATH" -jar "$JAR_FILE_PATH"
