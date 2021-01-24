cf48d434087a3e81f6a289b4753e853a07f02164
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-01-24 19:26:48 +0100

    Code refactoring

--- bin/posmulten-ddl.sh
+++ bin/posmulten-ddl.sh
@@ -10,4 +10,4 @@ CREATE_SCRIPT_PATH="$2"
 DROP_SCRIPT_PATH="$3"
 JAR_FILE_PATH="$SCRIPT_DIR/../lib/configuration-jar-0.3.0-jar-with-dependencies.jar"
 
-java -Dposmulten.configuration.config.file.path="$1" -Dposmulten.configuration.create.script.path="$CREATE_SCRIPT_PATHl" -Dposmulten.configuration.drop.script.path="$DROP_SCRIPT_PATH" -jar "$JAR_FILE_PATH"
+java -Dposmulten.configuration.config.file.path="$1" -Dposmulten.configuration.create.script.path="$CREATE_SCRIPT_PATH" -Dposmulten.configuration.drop.script.path="$DROP_SCRIPT_PATH" -jar "$JAR_FILE_PATH"
